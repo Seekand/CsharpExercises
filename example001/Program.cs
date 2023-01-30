1160574ce115eaa72fc4a88926a88e9cdd84a159
Daniel <teleportaefoda@gmail.com>
2023-01-30 06:14:42 -0300

    Exercises one was completed, and i initiated a other exercise

---
+++ example001/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sumtwonumbers{
+
+    class SumTwoNumbers{
+
+        static void Main(string[] args){
+            int n1, n2;
+
+            Console.WriteLine("Inform a number: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Inform a other number: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Clear();
+            Console.Write($"The numbers reporte were {n1} and {n2}, result = {n1+n2}");
+            Console.Read();
+            Console.Clear();
+        }
+    }
+}
