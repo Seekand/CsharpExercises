cc285e24bc5eb34ba7d4355542ed5d6b76fd60fb
Daniel <teleportaefoda@gmail.com>
2023-01-30 07:55:47 -0300

    Exercise 2 has been completed

--- example002/Program.cs
+++ example002/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+namespace exercise02{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Int32 number;
+            Console.WriteLine("Inform a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i < 11; i++){
+                Console.WriteLine($"{i} X {number} = {i * number}");
+            }
+        }
+    }
+}
